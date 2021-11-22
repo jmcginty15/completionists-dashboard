57c6f1771b07230f9b316ea793752848050bafd9
Jason McGinty <jason_mcginty@yahoo.com>
2021-11-21 23:01:18 -0600

    correcting cors origin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({ origin: 'http://ogbcompletionists.surge.sh' }));
+app.use(cors({ origin: 'https://ogbcompletionists.surge.sh' }));
 // app.use(cors({ origin: '*' }));
 
 app.use(express.json());
